a923f774cc0281fffa6db1e10c82873abf1925b9
MiguelMartinez007 <MiguelMartinezDW@outlook.com>
2019-08-08 04:44:28 -0500

    Cargo mensaje en el input y los muestro en el contenedor chat

--- public/main.js
+++ public/main.js
@@ -1,1 +1,12 @@
-alert('chat iniciado');
+$(function () {
+    console.log('Cargado!');
+
+    var message = $('#chat-message');
+
+    var chat = $('#chat');
+
+    $('#message-box').submit(function(e) {
+        e.preventDefault();
+        chat.append(message.val() + '<br>');
+    });
+});
